0c7cc9eaee36128ac3735f041f5e43da486a4a08
PING NGE <ping.nge@hotmail.com>
2021-10-06 00:36:14 +1100

    Python: grains finished

--- python/grains/grains.py
+++ python/grains/grains.py
@@ -1,6 +1,11 @@
+"""" Grains in a Chessboard """
+
+
 def square(number):
-    pass
+    if not 1 <= number <= 64:
+        raise ValueError("Square must be between 1 and 64")
+    return 1 << (number - 1)
 
 
 def total():
-    pass
+    return (1 << 64) - 1
