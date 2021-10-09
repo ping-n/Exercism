5f234e9b031f9e37a539a146dd79d66605f8cb47
PING NGE <ping.nge@hotmail.com>
2021-10-09 16:32:18 +1100

    Python: grains added comments

--- python/grains/grains.py
+++ python/grains/grains.py
@@ -2,10 +2,17 @@
 
 
 def square(number):
+    """
+        :param number = current square number
+        :return:  int input with current number of grain in on that sqaure
+    """
     if not 1 <= number <= 64:
         raise ValueError("Square must be between 1 and 64")
     return 1 << (number - 1)
 
 
 def total():
+    """
+        :return:  int return total number of grains
+    """
     return (1 << 64) - 1
