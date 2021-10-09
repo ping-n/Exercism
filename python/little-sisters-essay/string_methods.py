3137e930a14c5356c55e2d81bf30ad1e08fa5ec2
PING NGE <ping.nge@hotmail.com>
2021-10-09 16:09:08 +1100

    Python: little-sister-essay finished

--- python/little-sisters-essay/string_methods.py
+++ python/little-sisters-essay/string_methods.py
@@ -1,34 +1,33 @@
-def capitalize_title(title):
+def capitalize_title(title: str):
     """
 
     :param title: str title string that needs title casing
     :return:  str title string in title case (first letters capitalized)
     """
 
-    pass
+    return " ".join(word.capitalize() for word in title.split())
 
 
-def check_sentence_ending(sentence):
+def check_sentence_ending(sentence: str):
     """
 
     :param sentence: str a sentence to check.
     :return:  bool True if punctuated correctly with period, False otherwise.
     """
+    return True if sentence[-1] == '.' else False
 
-    pass
 
-
-def clean_up_spacing(sentence):
+def clean_up_spacing(sentence: str):
     """
 
     :param sentence: str a sentence to clean of leading and trailing space characters.
     :return: str a sentence that has been cleaned of leading and trailing space characters.
     """
 
-    pass
+    return sentence.strip()
 
 
-def replace_word_choice(sentence, old_word, new_word):
+def replace_word_choice(sentence: str, old_word: str, new_word: str):
     """
 
     :param sentence: str a sentence to replace words in.
@@ -37,4 +36,4 @@ def replace_word_choice(sentence, old_word, new_word):
     :return:  str input sentence with new words in place of old words
     """
 
-    pass
+    return sentence.replace(f"{old_word}", f"{new_word}")
